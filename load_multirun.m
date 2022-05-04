ce3fd730d7452dbb0a5dd2e51226404a89c42310
TedMcManus <edward.w.mcmanus.22@dartmouth.edu>
2022-05-03 22:18:10 -0400

    Update load_multirun
    It works now I guess - some sort of type conversion error is patched

--- load_multirun.m
+++ load_multirun.m
@@ -8,19 +8,22 @@ tic
 output_struct=struct();
 
 for i = 1:length(runlist)
-    direc=char(runlist(i));
-    if exist([direc,filesep,'keogram_data.mat'],'file') 
-        load([direc,filesep,'keogram_data.mat']);
+    dataloc=char(runlist(i));
+    if strcmp(dataloc(1:2),'./')
+        dataloc=dataloc(3:end);
+    end
+    if exist([dataloc,filesep,'keogram_data.mat'],'file') 
+        load([dataloc,filesep,'keogram_data.mat']);
         %Use dynamic field indexing to put all the data from keogram_data.mat into a struct
-        output_struct.(runlist(i)).('dens_95')=dens_95;
-        output_struct.(runlist(i)).('dens_120')=dens_120;
-        output_struct.(runlist(i)).('dens_300')=dens_300;
-        output_struct.(runlist(i)).('dens_800')=dens_800;
-        output_struct.(runlist(i)).('V_2')=V_2;
-        output_struct.(runlist(i)).('V_3')=V_3;
-        output_struct.(runlist(i)).('Jpar')=J_par;
-        output_struct.(runlist(i)).('SigmaH')=SigmaH;
-        output_struct.(runlist(i)).('SigmaP')=SigmaP;
+        output_struct.(dataloc).('dens_95')=dens_95;
+        output_struct.(dataloc).('dens_120')=dens_120;
+        output_struct.(dataloc).('dens_300')=dens_300;
+        output_struct.(dataloc).('dens_800')=dens_800;
+        output_struct.(dataloc).('V_2')=V_2;
+        output_struct.(dataloc).('V_3')=V_3;
+        output_struct.(dataloc).('Jpar')=J_par;
+        output_struct.(dataloc).('SigmaH')=SigmaH;
+        output_struct.(dataloc).('SigmaP')=SigmaP;
     end
 end
 
